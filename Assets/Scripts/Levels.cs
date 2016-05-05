49592a12410e7ae78e62f411a0b2c9ae80eafa41
Diego Gerena <dagerenaq@gmail.com>
2016-05-04 19:53:23 -0500

    Basic Level Script Complete
    Se completó el script de nivel, falta agregr las matrices de los
    diferentes niveles y los demas botones. Los botones se deben bloquear
    cada vez que se abre una ventana. Se revisará una manera más eficiente.

--- Assets/Scripts/Levels.cs
+++ Assets/Scripts/Levels.cs
@@ -3,10 +3,18 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class Levels : MonoBehaviour {
-	private int[,] l1_secuece;
-	private string[,] l1_message;
+	public Text dialog;
 
-	private string[] buttons = new string[]{"","destornillador","antivirus","cargador"};
+
+	private int[,] l1_secuece = { {1,2,3},
+								  {1,0,0},
+								  {0,1,0},
+								  {1,1,1},};
+	
+	private string[,] l1_message = {{"Bien","Bien","Bien"}, 
+									{"Mal","Mal","Mal"}};
+
+	private string[] buttons = new string[]{"","Voltimetro","Cargador","Memoria"};
 
 	private int[,] secuece;
 	private string[,] message;
@@ -14,30 +22,51 @@ public class Levels : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		int level = PlayerPrefs.GetInt ("Level");
+		int level=1;
+		if( PlayerPrefs.HasKey("Level") )
+			level = PlayerPrefs.GetInt ("Level");
 		if (level == 1) {
 			secuece = l1_secuece;
 			message = l1_message;
 		}
-		blockButtons ();
+		//blockButtons ();
 	
 	}
 
-	private void blockButtons(){
+	public void blockButtons(){
 		Button boton;
 		for (int c = 1; c < buttons.GetLength(0); c++) {
-			boton = GameObject.Find (buttons[c]).GetComponent<Button>();
-			boton.interactable = secuece[state,c] == 1;
+			try 
+			{
+				
+				boton = GameObject.Find (buttons[c]).GetComponent<Button>();
+				boton.interactable = secuece[c,state] == 1;
+			}
+			catch(System.Exception e)
+			{
+				Debug.Log (e.Message + '@'+ buttons [c] );
+			}
 		}
 	}
 
-	public void check(int buttonId, Text dialog){
-		if (secuece [state,0] == buttonId) {
-			dialog.text = message [state,0];
+	public void check(Button boton){
+		int buttonId = 0;
+		for (int c = 1; c < buttons.GetLength(0); c++) {
+			if (buttons [c] == boton.name) {
+				buttonId = c;
+				break;
+			}
+		}
+
+		
+		if (secuece [0,state] == buttonId) {
+			Debug.Log (message [0, state]);
+			//dialog.text = message [0,state];
 			state++;
 			blockButtons ();
 		} else {
-			dialog.text = message [state,1];
+			Debug.Log (message [1, state]);
+			//dialog.text = message [1,state];
 		}
 	}
 
